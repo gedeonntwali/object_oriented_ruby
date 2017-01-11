68c48a55fb738743ecf2edb5cdc31083470cabc5
Gedeon Ntwali <ntwari28@gmail.com>
2017-01-11 14:31:45 +0200

    added My store item with the description of the item

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+Product1 = {name: "shoe", color: "white", brand: "adidas", price: 1000}
+Product2 = {"name" => "car", "color" => "red", "make" => "Mercidic Benz", "price" => 15000}
+Product3 = {name: "computer", color: "grey", brand: "Mac", price: 1000}
+
+p Product2["name"]
