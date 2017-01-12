09938a318180c0ba7d096cdd443634ec638f387a
Gedeon Ntwali <ntwari28@gmail.com>
2017-01-12 11:18:31 +0200

    Refactoring by adding attribute reader and attribute writer

--- shop.rb
+++ shop.rb
@@ -1,37 +1,16 @@
 class Shop
+  
+  attr_reader :name, :color, :brand, :price
+  attr_writer :brand, :price
+
+
+
   def initialize(name, color, brand, price)
     @name = name
     @color = color
     @brand = brand
     @price = price
   end
-
-  def name
-    return @name
-  end
-
-  def color
-    return @color
-  end
-
-  
-  def brand=(brand)
-    return @brand
-  end
-
-  def brand
-    return @brand
-  end
-
-
-  def price
-    return @price
-  end
-
-  def price=(price)
-    return @price
-  end
-
 
   def info
     return "In my shop, I have  #{name} made by #{brand} and it has a #{color} color. If you want to buy it, it cost #{price}"
@@ -40,6 +19,7 @@ end
 
 shop = Shop.new("car", "black", "Ferrarri", 15000)
 puts shop.info
-shop.brand=("Bugatti") 
-shop.price= 20000
-puts shop.brand=("Bugatti") 
+shop.brand = "Bugatti"
+shop.price = 20000
+puts shop.brand
+puts shop.info
